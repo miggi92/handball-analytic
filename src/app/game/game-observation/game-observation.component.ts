e4c2cf114334aef8b00e42e00879c2207f3ec40d
Miguel Gebhardt <miguel@mgm12.de>
2022-02-22 20:45:35 +0100

    added click event

--- src/app/game/game-observation/game-observation.component.ts
+++ src/app/game/game-observation/game-observation.component.ts
@@ -1,18 +1,27 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { SnackService } from 'src/app/services/snack.service';
 
 @Component({
   selector: 'app-game-observation',
   templateUrl: './game-observation.component.html',
-  styleUrls: ['./game-observation.component.scss']
+  styleUrls: ['./game-observation.component.scss'],
 })
 export class GameObservationComponent implements OnInit {
-
   @Input() game;
   displayedColumns: string[] = ['number', 'name', 'actionsColumn'];
 
-  constructor() { }
+  constructor(private snackBar: SnackService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClickEvent(eventType) {
+    switch (eventType) {
+      case 'goal':
+        break;
+
+      default:
+        this.snackBar.error(`Event "${eventType}" not implemented yet!`);
+        break;
+    }
   }
-
 }
