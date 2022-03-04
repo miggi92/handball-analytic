326681a2ee0b9be5627c8b525f1d2c18e1695c1f
Miguel Gebhardt <miguel@mgm12.de>
2022-03-04 18:06:34 +0100

    added team details

--- src/app/team/team-detail/team-detail.component.ts
+++ src/app/team/team-detail/team-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Team } from '../models/team.model';
+import { TeamDatabaseService } from '../services/team-database.service';
 
 @Component({
   selector: 'app-team-detail',
@@ -6,7 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-detail.component.scss'],
 })
 export class TeamDetailComponent implements OnInit {
-  constructor() {}
+  team: Team;
+  sub: Subscription = new Subscription();
+  teamID: string;
+  constructor(
+    private route: ActivatedRoute,
+    private teamDB: TeamDatabaseService
+  ) {
+    this.route.params.subscribe((params) => {
+      this.teamID = params['teamId'];
+    });
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.sub = this.teamDB
+      .getTeam(this.teamID)
+      .subscribe((team) => (this.team = team));
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
