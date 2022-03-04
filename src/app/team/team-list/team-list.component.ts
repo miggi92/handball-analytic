ded820f96b29d3208a245a9543f7de5b893c2e28
Miguel Gebhardt <miguel@mgm12.de>
2022-03-04 17:15:45 +0100

    added team list entry

--- src/app/team/team-list/team-list.component.ts
+++ src/app/team/team-list/team-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Team } from '../models/team.model';
 
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
-  styleUrls: ['./team-list.component.scss']
+  styleUrls: ['./team-list.component.scss'],
 })
 export class TeamListComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'actionsColumn'];
+  teams: Team[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
