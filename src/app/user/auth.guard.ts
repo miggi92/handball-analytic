c647dc2df9f1d05e8d28ffdbd1742e94eb119243
Miguel Gebhardt <miguel@mgm12.de>
2022-03-12 10:28:56 +0100

    pretty printed

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,34 +1,33 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { SnackService } from '../services/snack.service';
 import { AuthService } from './services/auth.service';
 import { Observable, of } from 'rxjs';
 import { tap, map, take } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-  constructor(
-    private auth: AuthService,
-    private snack: SnackService
-  ) {}
+  constructor(private auth: AuthService, private snack: SnackService) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> {
-
-      return this.auth.user$.pipe(
-        take(1),
-        map(user => !!user), // <-- map to boolean
-        tap(loggedIn => {
-          if (!loggedIn) {
-            console.log('access denied')
-           this.snack.authError();
-          }
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.auth.user$.pipe(
+      take(1),
+      map((user) => !!user), // <-- map to boolean
+      tap((loggedIn) => {
+        if (!loggedIn) {
+          console.log('access denied');
+          this.snack.authError();
+        }
       })
     );
   }
-
 }
