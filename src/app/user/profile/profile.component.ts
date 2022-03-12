3e79a228189c0826f6f7619186431f3af47c7b02
Miguel Gebhardt <miguel@mgm12.de>
2022-03-12 12:05:38 +0100

    added version

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { environment } from '../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { Subscription } from 'rxjs';
@@ -8,27 +9,26 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  version = environment.version;
   clubs: Club[] = [];
-  sub: Subscription = new Subscription;
+  sub: Subscription = new Subscription();
 
-  constructor(public auth: AuthService, private clubDB: ClubDatabaseService) { }
+  constructor(public auth: AuthService, private clubDB: ClubDatabaseService) {}
 
   ngOnInit(): void {
     this.sub = this.clubDB
       .getUserClubs()
-      .subscribe(clubs => (this.clubs = clubs));
+      .subscribe((clubs) => (this.clubs = clubs));
   }
 
-  changedActiveClub(event: MatSlideToggleChange){
-    if(event.checked){
+  changedActiveClub(event: MatSlideToggleChange) {
+    if (event.checked) {
       this.auth.changeActiveClub(event.source.id);
-    } else{
-      this.auth.changeActiveClub("");
+    } else {
+      this.auth.changeActiveClub('');
     }
-
   }
-
 }
