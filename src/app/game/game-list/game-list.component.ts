b5dea8420cd164bfcd5a2b0f777ecfd6a57b81be
Miguel Gebhardt <miguel@mgm12.de>
2022-04-07 16:42:41 +0200

    fixed game list

--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -8,40 +8,38 @@ import { GameDatabaseService } from '../services/game-database.service';
 @Component({
   selector: 'app-game-list',
   templateUrl: './game-list.component.html',
-  styleUrls: ['./game-list.component.scss']
+  styleUrls: ['./game-list.component.scss'],
 })
 export class GameListComponent implements OnInit {
   displayedColumns: string[] = ['date', 'opponent', 'actionsColumn'];
   games: Game[] = [];
-  sub: Subscription = new Subscription;
+  sub: Subscription = new Subscription();
 
-  constructor(private gameDB: GameDatabaseService,  public dialog: MatDialog) { }
+  constructor(private gameDB: GameDatabaseService, public dialog: MatDialog) {}
 
   ngOnInit(): void {
-    this.sub = this.gameDB
-      .getClubGames()
-      .subscribe(games => this.games = games);
+    this.sub = this.gameDB.getClubGames().subscribe((games) => {
+      console.log('games', games);
+      this.games = games;
+    });
   }
 
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
 
-  openGameDialog (){
+  openGameDialog() {
     const dialogRef = this.dialog.open(GameCreateDialogComponent, {
       width: '400px',
-      data: {  }
+      data: {},
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.gameDB.createGame(result);
       }
     });
   }
 
-  deleteGame(element){
-
-  }
-
+  deleteGame(element) {}
 }
--- src/app/game/services/game-database.service.ts
+++ src/app/game/services/game-database.service.ts
@@ -7,47 +7,38 @@ import { AuthService } from 'src/app/user/services/auth.service';
 import { Game } from '../models/game.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class GameDatabaseService extends DefaultServiceService{
-  override _collection: string = "games";
+export class GameDatabaseService extends DefaultServiceService {
+  override _collection: string = 'games';
 
-  getGames(){
-    return this.db.collection(this._collection).valueChanges({idField: 'id'});
+  getGames() {
+    return this.db.collection(this._collection).valueChanges({ idField: 'id' });
   }
 
-  getGame(gameId){
-    return this.db.collection(this._collection).doc(gameId).valueChanges({idField: 'id'});
+  getGame(gameId) {
+    return this.db
+      .collection(this._collection)
+      .doc(gameId)
+      .valueChanges({ idField: 'id' });
   }
 
-  getClubGames(){
-      return this.auth.user$.pipe(
-        switchMap((user) => {
-          if (user) {
-            return this.db
-              .collection<Game>(this._collection, (ref) =>
-                ref.where('clubId', '==', this.db.collection('clubs').doc(this._userData.activeClub).ref)
-                  .orderBy('name')
-              )
-              .valueChanges({
-                idField: 'id',
-              });
-          } else {
-            return [];
-          }
-        }));
-  }
-
-  getUserGames(){
+  getClubGames() {
     return this.auth.user$.pipe(
-      switchMap(user => {
+      switchMap((user) => {
         if (user) {
           return this.db
-            .collection<Game>(this._collection, ref =>
-              ref.where('created.by', '==', user.uid).orderBy('name')
+            .collection<Game>(this._collection, (ref) =>
+              ref
+                .where(
+                  'clubId',
+                  '==',
+                  this.db.collection('clubs').doc(this._userData.activeClub).ref
+                )
+                .orderBy('date')
             )
             .valueChanges({
-              idField: 'id'
+              idField: 'id',
             });
         } else {
           return [];
@@ -56,22 +47,37 @@ export class GameDatabaseService extends DefaultServiceService{
     );
   }
 
-  async createGame(data: Game){
+  getUserGames() {
+    return this.auth.user$.pipe(
+      switchMap((user) => {
+        if (user) {
+          return this.db
+            .collection<Game>(this._collection, (ref) =>
+              ref.where('created.by', '==', this._userReference).orderBy('name')
+            )
+            .valueChanges({
+              idField: 'id',
+            });
+        } else {
+          return [];
+        }
+      })
+    );
+  }
+
+  async createGame(data: Game) {
     const user = await this.auth.getUser();
     return this.db.collection(this._collection).add({
       ...data,
-      clubId: user.activeClub,
+      clubId: this.db.collection('clubs').doc(user.activeClub).ref,
       created: {
-        by: user.uid,
-        at: Date.now()
-      }
+        by: this._userReference,
+        at: Date.now(),
+      },
     });
   }
 
-  deleteGame(gameId: string){
-    return this.db
-      .collection(this._collection)
-      .doc(gameId)
-      .delete();
+  deleteGame(gameId: string) {
+    return this.db.collection(this._collection).doc(gameId).delete();
   }
 }
