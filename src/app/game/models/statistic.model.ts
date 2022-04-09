3abf6fae8ecb5ac8231d506f531560d92e7634cd
Miguel Gebhardt <miguel@mgm12.de>
2022-04-10 00:44:27 +0200

    added goalkeeper logic

--- src/app/game/models/statistic.model.ts
+++ src/app/game/models/statistic.model.ts
@@ -7,6 +7,8 @@ export interface Statistic {
   playerId?: string;
   goals?: number;
   missed?: number;
+  getGoal?: number;
+  saved?: number;
   hpi?: number;
 }
 
@@ -27,7 +29,6 @@ export class calcStatistic {
   selectedTeam: string;
   historyEntry: HistoryEntry;
   gameDB: GameDatabaseService;
-  keeperStats: Statistic;
 
   constructor(
     selectedPlayer: Player,
@@ -51,77 +52,123 @@ export class calcStatistic {
         this.gameDB.updateStatistics(this.game.id, this.game.statistics);
         return;
       }
-      let index = -1;
-      var team = this.game.statistics[this.selectedTeam];
-      let stats: Statistic = team.find(
-        (element) => element.playerId === this.selectedPlayer.id
-      );
-      this.getKeeper();
 
-      if (stats) {
-        index = team.indexOf(stats);
-      } else {
-        stats = this.initPlayerStat(this.selectedPlayer.id);
-      }
+      this.keeperStats();
+      this.playerStats();
 
-      switch (this.eventType) {
-        case EventType.goal:
-          stats.goals++;
-          this.changeHPI(this.eventType, stats);
-          break;
-        case EventType.missed:
-          stats.missed++;
-          this.changeHPI(this.eventType, stats);
-          break;
-        default:
-          break;
-      }
-      if (index !== -1) {
-        team[index] = stats;
-      } else {
-        team.push(stats);
-      }
-      this.historyEntry = {
-        date: new Date(),
-        event: this.eventType,
-        teamId: this.selectedTeam,
-        playerId: this.selectedPlayer.id,
-      };
-      this.game.statistics[this.selectedTeam] = team;
-      this.game.statistics.history.push(this.historyEntry);
       this.gameDB.updateStatistics(this.game.id, this.game.statistics);
     }
   }
 
-  private getKeeper() {
-    let id;
-    if (
-      this.selectedTeam === 'home' &&
-      this.game.statistics.away &&
-      this.game.statistics.activeKeeper.away
-    ) {
-      this.keeperStats = this.game.statistics.away.find(
-        (element) => element.playerId === this.game.statistics.activeKeeper.away
-      );
-      id = this.game.statistics.activeKeeper.away;
-    } else if (
-      this.game.statistics.home &&
-      this.game.statistics.activeKeeper.home
-    ) {
-      this.keeperStats = this.game.statistics.home.find(
-        (element) => element.playerId === this.game.statistics.activeKeeper.home
-      );
-      id = this.game.statistics.activeKeeper.home;
+  private getOpponentTeam() {
+    if (this.selectedTeam === 'home') {
+      return 'away';
+    } else {
+      return 'home';
     }
-    if (!this.keeperStats && id) {
+  }
+
+  private keeperStats() {
+    let id;
+    let keeper: Statistic;
+    let index = -1;
+    let opponentTeam = this.getOpponentTeam();
+    var team = this.game.statistics[opponentTeam];
+
+    if (!team) {
+      return;
+    }
+
+    if (
+      this.game.statistics[opponentTeam] &&
+      this.game.statistics.activeKeeper[opponentTeam]
+    ) {
+      id = this.game.statistics.activeKeeper[opponentTeam];
+      keeper = this.game.statistics[opponentTeam].find(
+        (element) => element.playerId === id
+      );
+      index = team.indexOf(keeper);
+    }
+    if (!keeper && id) {
       this.initPlayerStat(this.selectedPlayer.id);
     }
+    if (!keeper) {
+      return;
+    }
+
+    switch (this.eventType) {
+      case EventType.goal:
+        keeper.getGoal++;
+        this.changeHPI(this.eventType, keeper);
+        break;
+      case EventType.missed:
+        keeper.saved++;
+        this.changeHPI(this.eventType, keeper);
+        break;
+      default:
+        break;
+    }
+    if (index !== -1) {
+      team[index] = keeper;
+    } else {
+      team.push(keeper);
+    }
+
+    this.game.statistics[opponentTeam] = team;
+    this.updateHistory(opponentTeam, keeper.playerId);
+  }
+
+  private playerStats() {
+    let index = -1;
+    var team = this.game.statistics[this.selectedTeam];
+    if (!team) {
+      return;
+    }
+    let stats: Statistic = team.find(
+      (element) => element.playerId === this.selectedPlayer.id
+    );
+    if (stats) {
+      index = team.indexOf(stats);
+    } else {
+      stats = this.initPlayerStat(this.selectedPlayer.id);
+    }
+
+    switch (this.eventType) {
+      case EventType.goal:
+        stats.goals++;
+        this.changeHPI(this.eventType, stats);
+        break;
+      case EventType.missed:
+        stats.missed++;
+        this.changeHPI(this.eventType, stats);
+        break;
+      default:
+        break;
+    }
+    if (index !== -1) {
+      team[index] = stats;
+    } else {
+      team.push(stats);
+    }
+    this.game.statistics[this.selectedTeam] = team;
+    this.updateHistory(this.selectedTeam, this.selectedPlayer.id);
+  }
+
+  private updateHistory(team, playerId) {
+    let historyEntry = {
+      date: new Date(),
+      event: this.eventType,
+      teamId: team,
+      playerId: playerId,
+    };
+    this.game.statistics.history.push(historyEntry);
   }
 
   private changeHPI(
     eventType,
     stats: Statistic,
-    goal: boolean = false
+    goal: boolean = false,
+    goalKeeper: boolean = false
   ): Statistic {
     let valueChange;
     let keeperValue;
@@ -155,12 +202,11 @@ export class calcStatistic {
       default:
         break;
     }
-    if (!valueChange) {
-      return stats;
-    }
-    stats.hpi = stats.hpi + valueChange;
-    if (this.keeperStats && keeperValue) {
-      this.keeperStats.hpi = this.keeperStats.hpi + keeperValue;
+
+    if (!goalKeeper && valueChange) {
+      stats.hpi = stats.hpi + valueChange;
+    } else if (keeperValue) {
+      stats.hpi = stats.hpi + keeperValue;
     }
     return stats;
   }
