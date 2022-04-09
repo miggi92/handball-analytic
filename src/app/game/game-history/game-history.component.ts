8ee1a9f4e0544b2576079bf46a74055917a8a188
Miguel Gebhardt <miguel@mgm12.de>
2022-04-09 16:28:06 +0200

    added history tab

--- src/app/game/game-history/game-history.component.ts
+++ src/app/game/game-history/game-history.component.ts
@@ -1,5 +1,12 @@
+import { Player } from './../../player/models/player.model';
+import { EventType } from './../models/statistic.model';
 import { Component, Input, OnInit } from '@angular/core';
 import { Game } from '../models/game.model';
+import {
+  faBasketball,
+  faQuestion,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-game-history',
@@ -8,7 +15,55 @@ import { Game } from '../models/game.model';
 })
 export class GameHistoryComponent implements OnInit {
   @Input() game: Game;
-  constructor() {}
+  customIcons = {
+    goal: faBasketball,
+    missed: faXmark,
+    unknown: faQuestion,
+  };
+  players: Player[];
+  displayedColumns: string[] = ['date', 'event', 'team', 'player'];
+  constructor() {
+    this.players = new Array<Player>();
+  }
 
   ngOnInit(): void {}
+
+  convertEvent2Icon(eventType) {
+    let icon;
+    switch (eventType) {
+      case EventType.goal:
+        icon = this.customIcons.goal;
+        break;
+      case EventType.missed:
+        icon = this.customIcons.missed;
+        break;
+
+      default:
+        icon = this.customIcons.unknown;
+        break;
+    }
+    return icon;
+  }
+  displayPlayerInfo(playerId) {
+    this.initPlayers();
+    let player: Player = this.players.find(
+      (element) => element.id === playerId
+    );
+    if (player) {
+      return `${player.number} - ${player.firstName} ${player.name}`;
+    } else {
+      return playerId;
+    }
+  }
+
+  initPlayers() {
+    if (this.game.players && this.players.length === 0) {
+      if (this.game.players.home && this.game.players.home[0].name) {
+        this.players = this.players.concat(this.game.players.home);
+      }
+      if (this.game.players.away && this.game.players.away[0].name) {
+        this.players = this.players.concat(this.game.players.away);
+      }
+    }
+  }
 }
