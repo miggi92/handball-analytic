f502e1a46946570654927191c7f86555ede04105
Miguel Gebhardt <miguel@mgm12.de>
2022-06-01 18:55:28 +0200

    removed test error

--- src/app/user/google-signin.directive.spec.ts
+++ src/app/user/google-signin.directive.spec.ts
@@ -1,8 +1,9 @@
 import { GoogleSigninDirective } from './google-signin.directive';
+import { AuthService } from './services/auth.service';
 
 describe('GoogleSigninDirective', () => {
   it('should create an instance', () => {
-    const directive = new GoogleSigninDirective();
+    const directive = new GoogleSigninDirective(null);
     expect(directive).toBeTruthy();
   });
 });
